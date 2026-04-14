76d537157219742d2debf7a663e84f8b51e35d7a
dgalluccio0 <daniele.galluccio0@gmail.com>
2026-04-14 09:36:43 +0200

    Fixed typos in the error messages.

--- src/main/java/io/github/dgalluccio0/usermanager/utils/Finals.java
+++ src/main/java/io/github/dgalluccio0/usermanager/utils/Finals.java
@@ -11,13 +11,13 @@ public interface Finals {
 	String EMAIL_REQUIRED_ERROR = "Email required.";
 	String EMAIL_NOT_VALID_ERROR = "Email not valid.";
 	String EMAIL_ALREADY_EXISTS_ERROR = "Email already exists.";
-	String EMAIL_TOO_LONG_ERROR = "Email must be at most" + MAX_EMAIL_LENGTH + "characters";
+	String EMAIL_TOO_LONG_ERROR = "Email must be at most " + MAX_EMAIL_LENGTH + " characters";
 
 	String USERNAME_REQUIRED_ERROR = "Username required.";
-	String USERNAME_TOO_LONG_ERROR = "Username must be at most" + MAX_USERNAME_LENGTH + "characters";
+	String USERNAME_TOO_LONG_ERROR = "Username must be at most " + MAX_USERNAME_LENGTH + " characters";
 
 	String PASSWORD_REQUIRED_ERROR = "Password required.";
-	String PASSWORD_TOO_LONG_ERROR = "Password must be at most" + MAX_PASSWORD_LENGTH + "characters";
+	String PASSWORD_TOO_LONG_ERROR = "Password must be at most " + MAX_PASSWORD_LENGTH + " characters";
 
 	String USER_NOT_FOUND_ERROR = "User not found.";
 }
